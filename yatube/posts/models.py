8bb3dabd05b6f8fb2bc9f4d574f07cb2f7535814
morgunovvb <74514540+morgunovvb@users.noreply.github.com>
2022-01-15 17:20:14 +0300

    Update models.py

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         max_length=50,
         related_name='group',
         blank=True,
